8ded05580dd7dd5945d8b076452eb7d89cc23746
Edarcox <eduardo.cabrera@alumnos.uv.cl>
2021-11-28 00:17:45 -0600

    Avanze Edu

---
+++ src/main/java/titec/proyecto/rest/Controller/FollowController.java
@@ -0,0 +1,52 @@
+package titec.proyecto.rest.Controller;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
+import java.net.MalformedURLException;
+
+import javax.ws.rs.Produces;
+import javax.ejb.Singleton;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.PATCH;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import titec.proyecto.rest.Model.EntitiesFollow.UsuarioFollow;
+import titec.proyecto.rest.Service.FollowService;
+
+@Path("/follow")
+@Singleton
+public class FollowController {
+    @Inject
+    private FollowService followService;
+    @Context
+    private UriInfo uriInfo;
+
+    @POST
+    @Path("/{id}")
+    @Consumes(APPLICATION_JSON)
+    @Produces(APPLICATION_JSON)
+    @Transactional
+    public Response addUsuario(@PathParam("id") long id,long id_seguidor){
+        Response.ResponseBuilder builder = null;
+        try {
+            // Store the contact
+            followService.insert(id,id_seguidor);
+            
+            // Create an "ok" response with the persisted contact
+            builder = Response.ok();
+        } catch (Exception e) {
+            // Handle generic exceptions
+            builder = Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage());
+        }
+
+        return builder.build();
+    }
+
+}
--- src/main/java/titec/proyecto/rest/Model/Categoria.java
+++ src/main/java/titec/proyecto/rest/Model/Categoria.java
@@ -15,7 +15,6 @@ import javax.persistence.UniqueConstraint;
 public class Categoria implements Serializable{
     private static final long serialVersionUID = 1L;
 
-
     @Id
     private long id;
 
