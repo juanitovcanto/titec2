46fa507204db83e998860805d91adf8b867c5f7a
juan <diego.valenzuela.canto@gmail.com>
2021-12-13 22:38:18 -0300

    arreglo contexto persistencia pais-categoria

--- src/main/java/titec/proyecto/rest/Service/impl/PaisServiceImpl.java
+++ src/main/java/titec/proyecto/rest/Service/impl/PaisServiceImpl.java
@@ -5,41 +5,34 @@ import titec.proyecto.rest.Service.PaisService;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.UriInfo;
 
 import titec.proyecto.rest.DTO.PaisDTO;
 import titec.proyecto.rest.Mapper.PaisMapper;
 import titec.proyecto.rest.Model.Paises;
 
 public class PaisServiceImpl implements PaisService {
-  @Context
-  UriInfo uriInfo;
-  
+
+  @PersistenceContext
+  private EntityManager em;
+
   @Override
   public List<PaisDTO> findAll(){
-        EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "primary" );
-        EntityManager entitymanager = emfactory.createEntityManager( );
-        CriteriaBuilder criteriaBuilder = entitymanager.getCriteriaBuilder();
+        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
         CriteriaQuery<Object> criteriaQuery = criteriaBuilder.createQuery();
         Root<Paises> from = criteriaQuery.from(Paises.class);
 
         CriteriaQuery<Object> select = criteriaQuery.select(from);
         
-        TypedQuery<Object> typedQuery = entitymanager.createQuery(select);
+        TypedQuery<Object> typedQuery = em.createQuery(select);
 
         List<Object> resultList = typedQuery.getResultList();
 
         List<PaisDTO> resultado = PaisMapper.convertToDTO(resultList);
-
-        entitymanager.close( );
-        emfactory.close( );
 
         return resultado;
   }
